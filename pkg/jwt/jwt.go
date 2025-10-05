953510dfc47447694795457e6d32ef4bd28a4496
Azmi Al Ghifari Rahman <azmiagr@gmail.com>
2025-10-05 23:28:44 +0700

    feat: add jwt for authentication

---
+++ pkg/jwt/jwt.go
@@ -0,0 +1,93 @@
+package jwt
+
+import (
+	"akademi-business-case/entity"
+	"errors"
+	"log"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
+)
+
+type Interface interface {
+	CreateJWTToken(userID uuid.UUID, isAdmin bool) (string, error)
+	ValidateToken(tokenString string) (uuid.UUID, error)
+	GetLoginUser(c *gin.Context) (*entity.User, error)
+}
+
+type jsonWebToken struct {
+	SecretKey   string
+	ExpiredTime time.Duration
+}
+
+type Claims struct {
+	user_id uuid.UUID
+	isAdmin bool
+	jwt.RegisteredClaims
+}
+
+func Init() Interface {
+	secretKey := os.Getenv("JWT_SECRET_KEY")
+	expiredTime, err := strconv.Atoi(os.Getenv("JWT_EXP_TIME"))
+	if err != nil {
+		log.Fatalf("error init jwt %v", err)
+	}
+
+	return &jsonWebToken{
+		SecretKey:   secretKey,
+		ExpiredTime: time.Duration(expiredTime) * time.Hour,
+	}
+}
+
+func (j *jsonWebToken) CreateJWTToken(userID uuid.UUID, isAdmin bool) (string, error) {
+	claims := &Claims{
+		user_id: userID,
+		isAdmin: isAdmin,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(j.ExpiredTime)),
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tokenString, err := token.SignedString([]byte(j.SecretKey))
+	if err != nil {
+		return "", err
+	}
+
+	return tokenString, nil
+}
+
+func (j *jsonWebToken) ValidateToken(tokenString string) (uuid.UUID, error) {
+	var (
+		claim  Claims
+		userID uuid.UUID
+	)
+
+	token, err := jwt.ParseWithClaims(tokenString, &claim, func(t *jwt.Token) (interface{}, error) {
+		return []byte(j.SecretKey), nil
+	})
+
+	if err != nil {
+		return userID, err
+	}
+
+	if !token.Valid {
+		return userID, errors.New("token is not valid")
+	}
+
+	userID = claim.user_id
+	return userID, nil
+}
+
+func (j *jsonWebToken) GetLoginUser(c *gin.Context) (*entity.User, error) {
+	user, ok := c.Get("user")
+	if !ok {
+		return &entity.User{}, errors.New("failed to get user login")
+	}
+
+	return user.(*entity.User), nil
+}

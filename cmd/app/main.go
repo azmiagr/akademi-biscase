d7060f273ae149d4c6fadd0a3dea303c0bbffb7c
Azmi Al Ghifari Rahman <azmiagr@gmail.com>
2025-10-05 23:33:04 +0700

    feat: add setup in main.go

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,1 +1,21 @@
 package main
+
+import (
+	"akademi-business-case/pkg/config"
+	"akademi-business-case/pkg/database/mariadb"
+	"log"
+)
+
+func main() {
+	config.LoadEnvironment()
+
+	db, err := mariadb.ConnectDatabase()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = mariadb.Migrate(db)
+	if err != nil {
+		log.Fatal(err)
+	}
+}

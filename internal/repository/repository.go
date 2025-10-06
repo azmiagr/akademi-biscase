3f4400a561c66f5096b4768f643d77f10c14cdaa
Azmi Al Ghifari Rahman <azmiagr@gmail.com>
2025-10-07 00:23:08 +0700

    feat: add user, otp, and cart dependencies

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -3,8 +3,15 @@ package repository
 import "gorm.io/gorm"
 
 type Repository struct {
+	UserRepository IUserRepository
+	OtpRepository  IOtpRepository
+	CartRepository ICartRepository
 }
 
 func NewRepository(db *gorm.DB) *Repository {
-	return &Repository{}
+	return &Repository{
+		UserRepository: NewUserRepository(db),
+		OtpRepository:  NewOtpRepository(db),
+		CartRepository: NewCartRepository(db),
+	}
 }

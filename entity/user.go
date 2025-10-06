e81af683f8a15de6bb03faa213e391c1ccf8f725
Azmi Al Ghifari Rahman <azmiagr@gmail.com>
2025-10-07 00:26:19 +0700

    feat: add status attributes to the user

--- entity/user.go
+++ entity/user.go
@@ -12,6 +12,7 @@ type User struct {
 	Username  string    `json:"username" gorm:"type:varchar(100);not null"`
 	Email     string    `json:"email" gorm:"type:varchar(50);uniqueIndex;not null"`
 	Password  string    `json:"password" gorm:"type:varchar(80);not null"`
+	Status    string    `json:"status" gorm:"type:enum('active','inactive');default:'inactive'"`
 	CreatedAt time.Time `json:"created_at" gorm:"autoCreateTime"`
 	UpdatedAt time.Time `json:"updated_at" gorm:"autoUpdateTime"`
 

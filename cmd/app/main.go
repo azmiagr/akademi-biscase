3e0de34d2a83932979549129b9b96bccea332d6f
Azmi Al Ghifari Rahman <azmiagr@gmail.com>
2025-10-06 22:28:04 +0700

    feat: add repository, service, bcrypt, and jwt dependencies to the main

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"akademi-business-case/internal/handler/rest"
+	"akademi-business-case/internal/repository"
+	"akademi-business-case/internal/service"
+	"akademi-business-case/pkg/bcrypt"
 	"akademi-business-case/pkg/config"
 	"akademi-business-case/pkg/database/mariadb"
+	"akademi-business-case/pkg/jwt"
 	"log"
 )
 
@@ -18,4 +23,13 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	repo := repository.NewRepository(db)
+	bcrypt := bcrypt.Init()
+	jwt := jwt.Init()
+	svc := service.NewService(repo, bcrypt, jwt)
+
+	r := rest.NewRest(svc)
+	r.MountEndpoint()
+	r.Run()
 }

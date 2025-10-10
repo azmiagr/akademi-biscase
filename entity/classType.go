8181bf878c35970d53d359b6cb85f17d5dbe06c3
Azmi Al Ghifari Rahman <azmiagr@gmail.com>
2025-10-11 01:49:47 +0700

    feat: add ClassType entity with fields for class type details and relationship to Classes

---
+++ entity/classType.go
@@ -0,0 +1,10 @@
+package entity
+
+import "github.com/google/uuid"
+
+type ClassType struct {
+	ClassTypeID uuid.UUID `json:"class_type_id" gorm:"type:varchar(36);primaryKey"`
+	Name        string    `json:"name" gorm:"type:enum('reguler', 'competition preparation', 'live bootcamp', 'private coaching');not null"`
+
+	Classes []Class `gorm:"foreignKey:ClassTypeID"`
+}

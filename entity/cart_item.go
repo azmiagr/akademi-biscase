ffbf8e69b297ce16db9f20a5bd03b8e78e2b06e6
Azmi Al Ghifari Rahman <azmiagr@gmail.com>
2025-10-11 01:49:29 +0700

    fix: rename ProductID to ClassID in CartItem struct for clarity

--- entity/cart_item.go
+++ entity/cart_item.go
@@ -9,7 +9,7 @@ import (
 type CartItem struct {
 	CartItemID uuid.UUID `json:"cartItemID" gorm:"type:varchar(36);primaryKey"`
 	CartID     uuid.UUID `json:"cartID" gorm:"type:varchar(36)"`
-	ProductID  uuid.UUID `json:"productID" gorm:"type:varchar(36)"`
+	ClassID    uuid.UUID `json:"productID" gorm:"type:varchar(36)"`
 	CreatedAt  time.Time `json:"createdAt" gorm:"autoCreateTime"`
 	UpdatedAt  time.Time `json:"updatedAt" gorm:"autoUpdateTime"`
 }

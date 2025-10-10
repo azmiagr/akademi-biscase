68e2ddabb42dbd3d5448d057b0dc44a7aba92e3e
Azmi Al Ghifari Rahman <azmiagr@gmail.com>
2025-10-10 15:51:48 +0700

    feat: update UserRegister struct to replace Username with FirstName and LastName fields

--- model/user.go
+++ model/user.go
@@ -10,7 +10,8 @@ type UserParam struct {
 }
 
 type UserRegister struct {
-	Username        string `json:"username" binding:"required"`
+	FirstName       string `json:"first_name" binding:"required"`
+	LastName        string `json:"last_name" binding:"required"`
 	Email           string `json:"email" binding:"required,email"`
 	Password        string `json:"password" binding:"required,min=8"`
 	ConfirmPassword string `json:"confirm_password" binding:"required"`

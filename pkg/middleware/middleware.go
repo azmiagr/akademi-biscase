1e626c39940286273da4ae6491ce9f5a6427aca2
Azmi Al Ghifari Rahman <azmiagr@gmail.com>
2025-10-10 15:52:38 +0700

    feat: implement timeout middleware for request handling and update dependencies

---
+++ pkg/middleware/middleware.go
@@ -0,0 +1,27 @@
+package middleware
+
+import (
+	"akademi-business-case/internal/service"
+	"akademi-business-case/pkg/jwt"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Interface interface {
+	AuthenticateUser(c *gin.Context)
+	OnlyAdmin(c *gin.Context)
+	Timeout() gin.HandlerFunc
+	Cors() gin.HandlerFunc
+}
+
+type middleware struct {
+	service *service.Service
+	jwtAuth jwt.Interface
+}
+
+func Init(service *service.Service, jwtAuth jwt.Interface) Interface {
+	return &middleware{
+		service: service,
+		jwtAuth: jwtAuth,
+	}
+}

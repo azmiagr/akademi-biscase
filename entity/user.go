8f0de8d4a5791a039dbe5cc6bb81687e706a46b7
Azmi Al Ghifari Rahman <azmiagr@gmail.com>
2025-10-10 15:51:25 +0700

    feat: update user struct to include first and last name fields

--- entity/user.go
+++ entity/user.go
@@ -9,7 +9,8 @@ import (
 type User struct {
 	UserID    uuid.UUID `json:"user_id" gorm:"type:varchar(36);primaryKey"`
 	RoleID    int       `json:"role_id"`
-	Username  string    `json:"username" gorm:"type:varchar(100);not null"`
+	FirstName string    `json:"first_name" gorm:"type:varchar(100);not null"`
+	LastName  string    `json:"last_name" gorm:"type:varchar(100);not null"`
 	Email     string    `json:"email" gorm:"type:varchar(50);uniqueIndex;not null"`
 	Password  string    `json:"password" gorm:"type:varchar(80);not null"`
 	Status    string    `json:"status" gorm:"type:enum('active','inactive');default:'inactive'"`

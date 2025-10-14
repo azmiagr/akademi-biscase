0c99c768837901250ac2ea74a98f863294c31c67
Azmi Al Ghifari Rahman <azmiagr@gmail.com>
2025-10-14 23:02:14 +0700

    feat: add ClassRepository to Repository struct for class management functionality

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -3,15 +3,17 @@ package repository
 import "gorm.io/gorm"
 
 type Repository struct {
-	UserRepository IUserRepository
-	OtpRepository  IOtpRepository
-	CartRepository ICartRepository
+	UserRepository  IUserRepository
+	OtpRepository   IOtpRepository
+	CartRepository  ICartRepository
+	ClassRepository IClassRepository
 }
 
 func NewRepository(db *gorm.DB) *Repository {
 	return &Repository{
-		UserRepository: NewUserRepository(db),
-		OtpRepository:  NewOtpRepository(db),
-		CartRepository: NewCartRepository(db),
+		UserRepository:  NewUserRepository(db),
+		OtpRepository:   NewOtpRepository(db),
+		CartRepository:  NewCartRepository(db),
+		ClassRepository: NewClassRepository(db),
 	}
 }

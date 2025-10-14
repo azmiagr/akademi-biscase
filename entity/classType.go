2d49aba71e9c17511dc2700be792d4cc4ff48535
Azmi Al Ghifari Rahman <azmiagr@gmail.com>
2025-10-14 23:01:20 +0700

    refactor: change ClassType Name field type from enum to varchar for improved flexibility

--- entity/classType.go
+++ entity/classType.go
@@ -4,7 +4,7 @@ import "github.com/google/uuid"
 
 type ClassType struct {
 	ClassTypeID uuid.UUID `json:"class_type_id" gorm:"type:varchar(36);primaryKey"`
-	Name        string    `json:"name" gorm:"type:enum('reguler', 'competition preparation', 'live bootcamp', 'private coaching');not null"`
+	Name        string    `json:"name" gorm:"type:varchar(100);not null"`
 
 	Classes []Class `gorm:"foreignKey:ClassTypeID"`
 }

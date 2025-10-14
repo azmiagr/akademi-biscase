d34d6cf8ba2985df558455b97691b9fa2a844bc2
Azmi Al Ghifari Rahman <azmiagr@gmail.com>
2025-10-14 23:01:07 +0700

    feat: update Class entity to include Discount field and adjust Price precision

--- entity/class.go
+++ entity/class.go
@@ -3,16 +3,16 @@ package entity
 import "github.com/google/uuid"
 
 type Class struct {
-	ClassID       uuid.UUID `json:"product_id" gorm:"type:varchar(36);primaryKey"`
-	ClassTypeID   uuid.UUID `json:"class_type_id"`
-	Name          string    `json:"name" gorm:"type:varchar(100);not null"`
-	Description   string    `json:"description" gorm:"type:text;default:null"`
-	Price         float64   `json:"price" gorm:"type:decimal(8,2);not null"`
-	OriginalPrice float64   `json:"original_price" gorm:"type:decimal(8,2);default:0"`
-	ImageURL      string    `json:"image_url" gorm:"type:varchar(255);default:null"`
-	Benefit       string    `json:"benefit" gorm:"type:text;default:null"`
-	TotalRating   float64   `json:"total_ratings" gorm:"type:decimal(2,1);default:0"`
-	TotalReview   int       `json:"total_reviews" gorm:"type:int;default:0"`
+	ClassID     uuid.UUID `json:"product_id" gorm:"type:varchar(36);primaryKey"`
+	ClassTypeID uuid.UUID `json:"class_type_id"`
+	Name        string    `json:"name" gorm:"type:varchar(100);not null"`
+	Description string    `json:"description" gorm:"type:text;default:null"`
+	Price       float64   `json:"price" gorm:"type:decimal(10,2);not null"`
+	Discount    float64   `json:"discount" gorm:"type:decimal(10,2);default:0"`
+	ImageURL    string    `json:"image_url" gorm:"type:varchar(255);default:null"`
+	Benefit     string    `json:"benefit" gorm:"type:text;default:null"`
+	TotalRating float64   `json:"total_ratings" gorm:"type:decimal(2,1);default:0"`
+	TotalReview int       `json:"total_reviews" gorm:"type:int;default:0"`
 
 	Reviews   []Review   `gorm:"foreignKey:ClassID"`
 	CartItems []CartItem `gorm:"foreignKey:ClassID"`

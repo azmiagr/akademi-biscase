6b8e8f4b96dbf79022614dbf72609e8588e3d815
Azmi Al Ghifari Rahman <azmiagr@gmail.com>
2025-10-14 23:02:40 +0700

    feat: add Class model with response structures for class management

---
+++ model/class.go
@@ -0,0 +1,31 @@
+package model
+
+import "github.com/google/uuid"
+
+type GetAllClassesResponse struct {
+	ClassID     uuid.UUID `json:"product_id" gorm:"type:varchar(36);primaryKey"`
+	Name        string    `json:"name" gorm:"type:varchar(100);not null"`
+	Description string    `json:"description" gorm:"type:text;default:null"`
+	Price       float64   `json:"price" gorm:"type:decimal(10,2);not null"`
+	Discount    float64   `json:"discount" gorm:"type:decimal(10,2);default:0"`
+	ImageURL    string    `json:"image_url" gorm:"type:varchar(255);default:null"`
+	TotalRating float64   `json:"total_ratings" gorm:"type:decimal(2,1);default:0"`
+	TotalReview int       `json:"total_reviews" gorm:"type:int;default:0"`
+}
+
+type ClassParam struct {
+	ClassID     uuid.UUID `json:"-"`
+	ClassTypeID uuid.UUID `json:"-"`
+	Name        string    `json:"-"`
+}
+
+type GetClassResponse struct {
+	ClassID     uuid.UUID `json:"product_id"`
+	Name        string    `json:"name"`
+	Description string    `json:"description"`
+	Price       float64   `json:"price"`
+	Discount    float64   `json:"discount"`
+	ImageURL    string    `json:"image_url"`
+	TotalRating float64   `json:"total_ratings"`
+	TotalReview int       `json:"total_reviews"`
+}

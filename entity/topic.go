04c00d05fada13bbb9ec72f80423bf920c33cff9
Azmi Al Ghifari Rahman <azmiagr@gmail.com>
2025-10-16 00:39:46 +0700

    feat: add Topic entity for class management, including fields for TopicID, ClassID, Name, and timestamps

---
+++ entity/topic.go
@@ -0,0 +1,15 @@
+package entity
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type Topic struct {
+	TopicID   uuid.UUID `json:"topic_id" gorm:"type:varchar(36);primaryKey"`
+	ClassID   uuid.UUID `json:"class_id"`
+	Name      string    `json:"name" gorm:"type:varchar(100);not null"`
+	CreatedAt time.Time `json:"created_at" gorm:"autoCreateTime"`
+	UpdatedAt time.Time `json:"updated_at" gorm:"autoUpdateTime"`
+}

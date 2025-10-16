7f9a71bb69851c38bccfd6983945a0092a416745
Azmi Al Ghifari Rahman <azmiagr@gmail.com>
2025-10-17 03:52:24 +0700

    feat: extend Repository struct to include TopicRepository and ContentRepository for enhanced content management

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -3,17 +3,21 @@ package repository
 import "gorm.io/gorm"
 
 type Repository struct {
-	UserRepository  IUserRepository
-	OtpRepository   IOtpRepository
-	CartRepository  ICartRepository
-	ClassRepository IClassRepository
+	UserRepository    IUserRepository
+	OtpRepository     IOtpRepository
+	CartRepository    ICartRepository
+	ClassRepository   IClassRepository
+	TopicRepository   ITopicRepository
+	ContentRepository IContentRepository
 }
 
 func NewRepository(db *gorm.DB) *Repository {
 	return &Repository{
-		UserRepository:  NewUserRepository(db),
-		OtpRepository:   NewOtpRepository(db),
-		CartRepository:  NewCartRepository(db),
-		ClassRepository: NewClassRepository(db),
+		UserRepository:    NewUserRepository(db),
+		OtpRepository:     NewOtpRepository(db),
+		CartRepository:    NewCartRepository(db),
+		ClassRepository:   NewClassRepository(db),
+		TopicRepository:   NewTopicRepository(db),
+		ContentRepository: NewContentRepository(db),
 	}
 }

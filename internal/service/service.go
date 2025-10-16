0e6f97e42e06fd3ab1cb25af4462bb4ae4e60726
Azmi Al Ghifari Rahman <azmiagr@gmail.com>
2025-10-17 03:52:52 +0700

    feat: add ContentService to Service struct for managing content and topic interactions

--- internal/service/service.go
+++ internal/service/service.go
@@ -7,15 +7,17 @@ import (
 )
 
 type Service struct {
-	UserService  IUserService
-	OtpService   IOtpService
-	ClassService IClassService
+	UserService    IUserService
+	OtpService     IOtpService
+	ClassService   IClassService
+	ContentService IContentService
 }
 
 func NewService(repository *repository.Repository, bcrypt bcrypt.Interface, jwtAuth jwt.Interface) *Service {
 	return &Service{
-		UserService:  NewUserService(repository.UserRepository, repository.CartRepository, repository.OtpRepository, bcrypt, jwtAuth),
-		OtpService:   NewOtpService(repository.OtpRepository, repository.UserRepository),
-		ClassService: NewClassService(repository.ClassRepository, repository.UserRepository),
+		UserService:    NewUserService(repository.UserRepository, repository.CartRepository, repository.OtpRepository, bcrypt, jwtAuth),
+		OtpService:     NewOtpService(repository.OtpRepository, repository.UserRepository),
+		ClassService:   NewClassService(repository.ClassRepository, repository.UserRepository),
+		ContentService: NewContentService(repository.ContentRepository, repository.TopicRepository, repository.ClassRepository),
 	}
 }

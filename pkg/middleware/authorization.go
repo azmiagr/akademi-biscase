66552d6a4d20d39e4286e9d6e702b64e58b442dc
Azmi Al Ghifari Rahman <azmiagr@gmail.com>
2025-10-16 22:48:11 +0700

    feat: add OnlyOperational middleware to restrict access based on user roles for enhanced security

--- pkg/middleware/authorization.go
+++ pkg/middleware/authorization.go
@@ -23,3 +23,20 @@ func (m *middleware) OnlyAdmin(c *gin.Context) {
 	}
 	c.Next()
 }
+
+func (m *middleware) OnlyOperational(c *gin.Context) {
+	user, err := m.jwtAuth.GetLoginUser(c)
+	if err != nil {
+		response.Error(c, http.StatusForbidden, "failed to get login user", err)
+		c.Abort()
+		return
+	}
+
+	if user.RoleID != 1 && user.RoleID != 4 {
+		response.Error(c, http.StatusForbidden, "this endpoint cannot be access", errors.New("user dont have access"))
+		c.Abort()
+		return
+	}
+
+	c.Next()
+}

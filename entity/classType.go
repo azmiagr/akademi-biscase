bd211710d14d5cfd5aa8678330e2b97e2e6fbc20
Azmi Al Ghifari Rahman <azmiagr@gmail.com>
2025-10-18 02:04:33 +0700

    fix: correct GORM tag syntax for ClassTypeID in ClassType struct

--- entity/classType.go
+++ entity/classType.go
@@ -3,7 +3,7 @@ package entity
 import "github.com/google/uuid"
 
 type ClassType struct {
-	ClassTypeID uuid.UUID `json:"class_type_id" gorm:"type:varchar(36);primaryKey"`
+	ClassTypeID uuid.UUID `json:"class_type_id" gorm:"type:varchar(36);primaryKey;"`
 	Name        string    `json:"name" gorm:"type:varchar(100);not null"`
 
 	Classes []Class `gorm:"foreignKey:ClassTypeID"`

4eede30b83b10187f326b0f853d5d662738a3391
Azmi Al Ghifari Rahman <azmiagr@gmail.com>
2025-10-18 02:05:10 +0700

    feat: enhance User struct with Title field and update relationships to include ClassMentors, EnrolledClasses, and Payments

--- entity/user.go
+++ entity/user.go
@@ -11,14 +11,17 @@ type User struct {
 	RoleID    int       `json:"role_id"`
 	FirstName string    `json:"first_name" gorm:"type:varchar(100);not null"`
 	LastName  string    `json:"last_name" gorm:"type:varchar(100);not null"`
+	Title     string    `json:"title" gorm:"type:varchar(50);default:null"`
 	Email     string    `json:"email" gorm:"type:varchar(50);uniqueIndex;not null"`
 	Password  string    `json:"password" gorm:"type:varchar(80);not null"`
 	Status    string    `json:"status" gorm:"type:enum('active','inactive');default:'inactive'"`
 	CreatedAt time.Time `json:"created_at" gorm:"autoCreateTime"`
 	UpdatedAt time.Time `json:"updated_at" gorm:"autoUpdateTime"`
 
-	Cart     Cart      `gorm:"foreignKey:UserID"`
-	OtpCodes []OtpCode `gorm:"foreignKey:UserID"`
-	Reviews  []Review  `gorm:"foreignKey:UserID"`
-	Classes  []Class   `gorm:"foreignKey:UserID"`
+	Cart            Cart            `gorm:"foreignKey:UserID"`
+	OtpCodes        []OtpCode       `gorm:"foreignKey:UserID"`
+	Reviews         []Review        `gorm:"foreignKey:UserID"`
+	ClassMentors    []ClassMentor   `gorm:"foreignKey:UserID"`
+	EnrolledClasses []EnrolledClass `gorm:"foreignKey:UserID"`
+	Payments        []Payment       `gorm:"foreignKey:UserID"`
 }

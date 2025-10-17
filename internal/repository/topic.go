a3f2a4401b758ac9685be4fa0839c287d189e058
Azmi Al Ghifari Rahman <azmiagr@gmail.com>
2025-10-18 02:06:05 +0700

    feat: add FindByNameAndClassID method to TopicRepository for retrieving topics by name and class ID

--- internal/repository/topic.go
+++ internal/repository/topic.go
@@ -4,12 +4,14 @@ import (
 	"akademi-business-case/entity"
 	"akademi-business-case/model"
 
+	"github.com/google/uuid"
 	"gorm.io/gorm"
 )
 
 type ITopicRepository interface {
 	CreateTopic(tx *gorm.DB, topic *entity.Topic) (*entity.Topic, error)
 	GetTopic(tx *gorm.DB, param model.GetTopicParam) (*entity.Topic, error)
+	FindByNameAndClassID(tx *gorm.DB, name string, classID uuid.UUID) (*entity.Topic, error)
 }
 
 type TopicRepository struct {
@@ -32,6 +34,16 @@ func (r *TopicRepository) CreateTopic(tx *gorm.DB, topic *entity.Topic) (*entity
 func (r *TopicRepository) GetTopic(tx *gorm.DB, param model.GetTopicParam) (*entity.Topic, error) {
 	var topic *entity.Topic
 	err := tx.Debug().Where(&param).First(&topic).Error
+	if err != nil {
+		return nil, err
+	}
+
+	return topic, nil
+}
+
+func (r *TopicRepository) FindByNameAndClassID(tx *gorm.DB, name string, classID uuid.UUID) (*entity.Topic, error) {
+	var topic *entity.Topic
+	err := tx.Debug().Where("name = ? AND class_id = ?", name, classID).First(&topic).Error
 	if err != nil {
 		return nil, err
 	}

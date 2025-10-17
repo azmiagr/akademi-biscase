0c16f3c8619defee3fea08cbd1829443a5e97a58
Azmi Al Ghifari Rahman <azmiagr@gmail.com>
2025-10-18 02:06:41 +0700

    feat: integrate ReviewService into Service struct and enhance ClassService initialization with ClassMentorRepository for improved class management

--- internal/service/service.go
+++ internal/service/service.go
@@ -11,13 +11,15 @@ type Service struct {
 	OtpService     IOtpService
 	ClassService   IClassService
 	ContentService IContentService
+	ReviewService  IReviewService
 }
 
 func NewService(repository *repository.Repository, bcrypt bcrypt.Interface, jwtAuth jwt.Interface) *Service {
 	return &Service{
 		UserService:    NewUserService(repository.UserRepository, repository.CartRepository, repository.OtpRepository, bcrypt, jwtAuth),
 		OtpService:     NewOtpService(repository.OtpRepository, repository.UserRepository),
-		ClassService:   NewClassService(repository.ClassRepository, repository.UserRepository),
+		ClassService:   NewClassService(repository.ClassRepository, repository.UserRepository, repository.ClassMentorRepository),
 		ContentService: NewContentService(repository.ContentRepository, repository.TopicRepository, repository.ClassRepository),
+		ReviewService:  NewReviewService(repository.ReviewRepository, repository.UserRepository, repository.ClassRepository),
 	}
 }
